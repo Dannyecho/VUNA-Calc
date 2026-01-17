ac7d5fcb67a8e38cd4da295ffd38f09bf6962b73
Simon Nungwa <simonnungwa99@gmail.com>
2026-01-17 11:31:57 +0100

    feat: Added keyboard support

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,61 @@ var operator = '';
 var right = '';
 let wordPlaceholder = document.getElementById('word-result');
 
+// Keyboard support
+document.addEventListener('keydown', function(event) {
+    const key = event.key;
+
+    
+    if (key >= '0' && key <= '9') {
+        appendToResult(key);
+        event.preventDefault();
+    }
+    else if (key === '.') {
+        appendToResult('.');
+        event.preventDefault();
+    }
+    else if (key === '+') {
+        operatorToResult('+');
+        event.preventDefault();
+    }
+    else if (key === '-') {
+        operatorToResult('-');
+        event.preventDefault();
+    }
+    else if (key === '*') {
+        operatorToResult('*');
+        event.preventDefault();
+    }
+    else if (key === '/') {
+        operatorToResult('/');
+        event.preventDefault();
+    }
+    // Enter or = for calculate
+    else if (key === 'Enter' || key === '=') {
+        calculateResult();
+        event.preventDefault();
+    }
+    // Backspace for delete
+    else if (key === 'Backspace') {
+        backspace();
+        event.preventDefault();
+    }
+    // Escape for clear
+    else if (key === 'Escape') {
+        clearResult();
+        event.preventDefault();
+    }
+    // Brackets
+    else if (key === '(') {
+        bracketToResult('(');
+        event.preventDefault();
+    }
+    else if (key === ')') {
+        bracketToResult(')');
+        event.preventDefault();
+    }
+});
+
 function appendToResult(value) {
     if (operator.length == 0) {
         left += value;
